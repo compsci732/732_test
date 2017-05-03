bd5ff691d231cefa4274de626909ded90b6fc6a4
Vikraman Mohan <vmoh364@aucklanduni.ac.nz>
2017-05-04 11:15:06 +1200

    MERGING SERVER LIBRARY

--- app/src/main/java/com/example/vikramanmohan/a702/Login.java
+++ app/src/main/java/com/example/vikramanmohan/a702/Login.java
@@ -10,46 +10,34 @@ import android.widget.TextView;
 
 public class Login extends AppCompatActivity implements View.OnClickListener {
 
-EditText etName,etEmail,etAge;
+    EditText etName, etEmail, etAge;
     Button logOutBtn;
-    localdb userLocalDB;
 
-    @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
         etName = (EditText) findViewById(R.id.etName);
 
         etAge = (EditText) findViewById(R.id.etAge);
         etEmail=(EditText) findViewById(R.id.etEmail);
 
         logOutBtn = (Button) findViewById(R.id.logOutBtn);
+
         logOutBtn.setOnClickListener(this);
-        userLocalDB = new localdb(this);
-
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-     /*   user user=userLocalDB.getLoggedInInfo();
-
-        etAge.setText(user.age);
-        etEmail.setText(user.email);
-        etName.setText(user.name);*/
-
     }
 
 
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.logOutBtn:
-                userLocalDB.clearUserData();
-                startActivity(new Intent(this,MainActivity.class));
 
+        switch (v.getId()) {
+            case R.id.logOutBtn:
+                startActivity(new Intent(this, MainActivity.class));
                 break;
         }
     }
 }
+
--- app/src/main/java/com/example/vikramanmohan/a702/MainActivity.java
+++ app/src/main/java/com/example/vikramanmohan/a702/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.vikramanmohan.a702;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,19 +9,30 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import org.fejoa.library.remote.SimpleJsonRemoteJob;
+import org.fejoa.library.support.Task;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.InetAddress;
+import java.net.MalformedURLException;
+import java.net.Socket;
+import java.net.URI;
+import java.net.URL;
+import java.net.URLConnection;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener  {
 
     Button loginBtn;
-    EditText etuserName,etPassword;
+    EditText etUserName,etPassword;
     TextView registerLink;
-    localdb localdb;
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.loginBtn:
-
                 startActivity(new Intent(this,Login.class));
+
                 break;
             case R.id.registerLink:
                 startActivity(new Intent(this,Sign_Up.class));
@@ -35,12 +47,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        etuserName = (EditText) findViewById(R.id.etUserName);
+        etUserName = (EditText) findViewById(R.id.etUserName);
         etPassword = (EditText) findViewById(R.id.etPassword);
         loginBtn = (Button) findViewById(R.id.loginBtn);
         registerLink = (TextView) findViewById(R.id.registerLink);
-
-        loginBtn.setOnClickListener(this);
         registerLink.setOnClickListener(this);
 
 
